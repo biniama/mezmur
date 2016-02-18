09e24359b994d50ed1e891c12d0147183a8f205b
Biniam Asnake Kefale <biniamasnake@gmail.com>
2016-02-18 23:05:36 +0100

    App able to run in prod

--- src/test/java/tech/hasset/MezmurApiApplicationTests.java
+++ src/test/java/tech/hasset/MezmurApiApplicationTests.java
@@ -10,7 +10,6 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = MezmurApiApplication.class)
 @WebAppConfiguration
-//@TestPropertySource(locations="classpath:application.yml")
 public class MezmurApiApplicationTests {
 
 	@Test
